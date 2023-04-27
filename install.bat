3cf4a11d0e21f6e989518d5f93bbacddaaad8b26
Brian Calivar <briancalivar@gmail.com>
2023-04-26 22:19:49 -0300

    Cambios menores en install.bat

--- install.bat
+++ install.bat
@@ -1,9 +1,3 @@
-@echo off
-
-set desired_version=3.10.2
-
-:: Verifica si Python ya está instalado
-where python >nul 2>nul
 if %errorlevel% neq 0 (
     echo Python no encontrado, instalando...
     goto InstallPython
@@ -15,30 +9,42 @@ if %errorlevel% neq 0 (
     echo Version actual de Python: %current_version%
 )
 
-:: Verifica si la version actual es igual o superior a la version deseada
-for /f "tokens=1-3 delims=." %%a in ("%current_version%") do set /a cv=%%a*10000 + %%b*100 + %%c
-for /f "tokens=1-3 delims=." %%a in ("%desired_version%") do set /a dv=%%a*10000 + %%b*100 + %%c
-
 if %cv% geq %dv% (
     echo La version actual de Python es igual o superior a %desired_version%, no se requiere actualizar
     goto UpdatePip
 ) else (
     echo Actualizando Python a la version %desired_version%...
+    pause
 )
 
 :InstallPython
 powershell -Command "& { iwr -useb https://www.python.org/ftp/python/%desired_version%/python-%desired_version%-amd64.exe -OutFile python-installer.exe }"
 .\python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+if %errorlevel% neq 0 (
+    echo Error al instalar Python
+    pause
+    exit /b 1
+)
 del .\python-installer.exe
 echo Python instalado correctamente
 
 :UpdatePip
 echo Actualizando pip...
 python -m pip install --upgrade pip
+if %errorlevel% neq 0 (
+    echo Error al actualizar pip
+    pause
+    exit /b 1
+)
 
 echo Instalando dependencias...
 python -m pip install -r requirements.txt
+if %errorlevel% neq 0 (
+    echo Error al instalar dependencias
+    pause
+    exit /b 1
+)
 
 echo.
 echo Instalacion completada. Presione cualquier tecla para salir.
-pause 
+pause
